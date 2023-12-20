8943d6a429337f4f5fd67b34f93a2fcf20e83a83
trietpham252 <142817355+trietpham252@users.noreply.github.com>
2023-12-20 21:48:03 +0700

    update

--- admin/src/pages/dashboard/index.tsx
+++ admin/src/pages/dashboard/index.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { Col, Image, Layout, Row } from 'antd';
+import Dashboard from '../dashboard'
+import Test from '../Tabs/Tabs'
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import Logo from '../../assets/images/Logo_Container.png'
-import { UserOutlined, SettingOutlined, BellOutlined, SearchOutlined, HomeOutlined, CustomerServiceOutlined, SafetyCertificateOutlined, KeyOutlined, TagsOutlined} from '@ant-design/icons';
+import {
+  UserOutlined,
+  SettingOutlined,
+  BellOutlined,
+  SearchOutlined,
+  HomeOutlined,
+  CustomerServiceOutlined,
+  SafetyCertificateOutlined,
+  KeyOutlined,
+  TagsOutlined,
+} from '@ant-design/icons';
 import SearchBar from '../../components/SeachBar';
 import ButtonSlider from '../../components/ButtonSlider';
-import './style.css'
-
+import './style.css';
+ 
 const { Header, Sider, Content } = Layout;
 
 const styleTextButton: React.CSSProperties = {
   width: 100,
-  textAlign: 'start'
-}
+  textAlign: 'start',
+};
 
 const styleIcon: React.CSSProperties = {
-  fontSize: '24px'
-}
+  fontSize: '24px',
+};
 
 const styleButton: React.CSSProperties = {
   width: 224,
@@ -24,8 +37,8 @@ const styleButton: React.CSSProperties = {
   display: 'flex',
   alignItems: 'center',
   border: 'none',
-  borderBottom: "1px solid  #F2F4F8",
-}
+  borderBottom: '1px solid  #F2F4F8',
+};
 
 const styleSearch: React.CSSProperties = {
   width: 224,
@@ -36,11 +49,36 @@ const styleSearch: React.CSSProperties = {
   justifyContent: 'space-between',
   border: 'none',
   borderRadius: 0,
-  borderBottom: "1px solid  #F2F4F8",
+  borderBottom: '1px solid  #F2F4F8',
   background: '#F2F4F8',
   marginTop: '20px',
-  marginBottom: '20px'
-}
+  marginBottom: '20px',
+};
+
+const buttonData = [
+  {
+    icon: <HomeOutlined style={styleIcon} />,
+    text: 'Dashboard',
+  },
+  {
+    icon: <CustomerServiceOutlined style={styleIcon} />,
+    text: 'Services',
+    isShowNotification: true,
+    isShowDropdown: true,
+  },
+  {
+    icon: <TagsOutlined style={styleIcon} />,
+    text: 'Plans',
+  },
+  {
+    icon: <KeyOutlined style={styleIcon} />,
+    text: 'API keys',
+  },
+  {
+    icon: <SafetyCertificateOutlined style={styleIcon} />,
+    text: 'Access Controls',
+  },
+];
 
 const Index: React.FC = () => (
   <Layout style={{ minHeight: '100vh' }}>
@@ -63,53 +101,33 @@ const Index: React.FC = () => (
       </Row>
       <SearchBar
         prefix={<SearchOutlined style={{ fontSize: 20 }} />}
-        placeholder='Search for...'
+        placeholder="Search for..."
         style={styleSearch}
       />
-      <ButtonSlider
-        type='text'
-        icon={<HomeOutlined style={styleIcon} />}
-        style={styleButton}
-        textbutton='Dashboard'
-      />
-
-      <ButtonSlider 
-        type='text'
-        icon={<CustomerServiceOutlined style={styleIcon} />}
-        isShowNotification={true}
-        isShowDropdown={true}
-        textbutton='Servcies'
-        styleTextButton={styleTextButton}
-        style={styleButton}
-      />
-
-<ButtonSlider
-        type='text'
-        icon={<TagsOutlined style={styleIcon} />}
-        style={styleButton}
-        textbutton='Plans'
-      />
-
-<ButtonSlider
-        type='text'
-        icon={<KeyOutlined style={styleIcon} />}
-        style={styleButton}
-        textbutton='API keys'
-      />
-
-<ButtonSlider
-        type='text'
-        icon={<SafetyCertificateOutlined style={styleIcon} />}
-        style={styleButton}
-        textbutton='Access Controls'
-      />
+      {buttonData.map((button, index) => (
+         <Link key={index} to={`/${button.text.toLowerCase()}`}> 
+         <ButtonSlider
+           key={index}
+           type="text"
+           icon={button.icon}
+           style={styleButton}
+           textbutton={button.text}
+           isShowNotification={button.isShowNotification}
+           isShowDropdown={button.isShowDropdown}
+           styleTextButton={styleTextButton}
+         />
+       </Link>
+      ))}
     </Sider>
     <Layout>
       <Header style={{ backgroundColor: '#4096ff', color: '#fff', textAlign: 'center' }}>
         Header
       </Header>
       <Content style={{ padding: '24px', minHeight: 280 }}>
-        Content
+      <Routes>
+             <Route path="/" element={<Dashboard />} />
+             <Route path="/tabs" element={<Test />} />
+      </Routes>
       </Content>
     </Layout>
   </Layout>
